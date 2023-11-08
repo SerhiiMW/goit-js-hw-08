f84e345bff9b58a9a112780a8b15341427631436
SerhiiMW <137871447+SerhiiMW@users.noreply.github.com>
2023-11-08 12:39:31 +0200

    Update3

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,6 +1,30 @@
 import Player from '@vimeo/player';
+import { saveToLS, loadFromLS } from './helpers';
+import throttle from 'lodash.throttle';
 
-const iframe = document.querySelector("iframe");
+const iframe = document.querySelector('iframe');
 
-const player = new Player(iframe)
+const player = new Player(iframe);
 
+function saveCurrentTime(event) {
+  let seconds = event.seconds;
+  const key = 'videoplayer-current-time';
+  const value = seconds;
+  saveToLS(key, value);
+}
+
+player.on('timeupdate', throttle(saveCurrentTime, 1000));
+
+let currentTime = loadFromLS('videoplayer-current-time');
+player
+  .setCurrentTime(currentTime)
+  .then(function (seconds) {})
+  .catch(function (error) {
+    switch (error.name) {
+      case 'RangeError':
+        break;
+
+      default:
+        break;
+    }
+  });
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -11,6 +11,15 @@ refs.formElem.addEventListener("input",
 )
 refs.formElem.addEventListener("submit", onFormSubmit)
 function onFormInput(event) {
+
+//     const key = 'feedback-form-state'
+//     const value = {
+//         eventName: event.target.name,
+//         eventValue: event.target.value,
+// }
+
+
+
     const key = event.target.name;
     const value = event.target.value;
     saveToLS(key, value)
@@ -18,16 +27,17 @@ function onFormInput(event) {
 function onFormSubmit(event) {
     event.preventDefault();
     const object = {
-        'feedback-form-state': {
+        // 'feedback-form-state': {
             email: refs.formElem.elements.email.value,
             message: refs.formElem.elements.message.value,
-        }
+        // }
     };
 
     console.log(object);
     event.target.reset();
     localStorage.removeItem('email');
     localStorage.removeItem('message');
+    // localStorage.removeItem('feedback-form-state');
 }
 function onLoad() {
     const email = loadFromLS('email');

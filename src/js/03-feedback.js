73082c1f59211fc6f63418a7c78372a8fc5e82af
SerhiiMW <137871447+SerhiiMW@users.noreply.github.com>
2023-11-08 13:12:04 +0200

    Update 03-feedback.js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -11,33 +11,23 @@ refs.formElem.addEventListener("input",
 )
 refs.formElem.addEventListener("submit", onFormSubmit)
 function onFormInput(event) {
-
-//     const key = 'feedback-form-state'
-//     const value = {
-//         eventName: event.target.name,
-//         eventValue: event.target.value,
-// }
-
-
-
-    const key = event.target.name;
-    const value = event.target.value;
+    const key = 'feedback-form-state'
+    const value = {
+        email: refs.formElem.elements.email.value,
+        message: refs.formElem.elements.message.value,
+}
     saveToLS(key, value)
 }
 function onFormSubmit(event) {
     event.preventDefault();
     const object = {
-        // 'feedback-form-state': {
             email: refs.formElem.elements.email.value,
             message: refs.formElem.elements.message.value,
-        // }
     };
 
     console.log(object);
     event.target.reset();
-    localStorage.removeItem('email');
-    localStorage.removeItem('message');
-    // localStorage.removeItem('feedback-form-state');
+    localStorage.removeItem('feedback-form-state');
 }
 function onLoad() {
     const email = loadFromLS('email');

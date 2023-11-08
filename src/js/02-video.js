32de353811331a4b3840161064eca53d628fa907
SerhiiMW <137871447+SerhiiMW@users.noreply.github.com>
2023-11-08 08:50:42 +0200

    Update 02-video.js

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,35 +1,6 @@
-import player from '@vimeo/player';
+import Player from '@vimeo/player';
 
-const iframe = document.querySelector('iframe');
-// const vimeo = document.getElementById('vimeo-player');
-const player = new Vimeo.Player(iframe);
+const iframe = document.querySelector("iframe");
 
-console.log(player);
-
-// player.on('play', function () {
-//   console.log('played the video!');
-// });
-
-// player.getVideoTitle().then(function (title) {
-//   console.log('title:', title);
-// });
-
-// player.on('timeupdate', function (data) {
-//     data = {
-//     duration: 0,
-//     percent: 0,
-//     seconds: 0
-// }
-// });
-
-    // const iframe = document.querySelector('iframe');
-    // const player = new Vimeo.Player(iframe);
-
-    // player.on('play', function() {
-    //     console.log('played the video!');
-    // });
-
-    // player.getVideoTitle().then(function(title) {
-    //     console.log('title:', title);
-    // });
+const player = new Player(iframe)
 
